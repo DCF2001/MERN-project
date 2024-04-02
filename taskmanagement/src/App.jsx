ad1c3e7be33b9d2134b6a1517b1f0676b184bde8
DCF2001 <154993535+DCF2001@users.noreply.github.com>
2024-04-02 17:45:23 +0530

    pages and routes are created

--- taskmanagement/src/App.jsx
+++ taskmanagement/src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Taskmanagement from "./pages/Taskmanagement"
+import Routerequests from "./pages/Routerequests"
+import Vehiclerequests from "./pages/Vehiclerequests"
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Taskmanagement />} />
+      <Route path="Routerequests" element={<Routerequests />} />
+      <Route path="Vehiclerequests" element={<Vehiclerequests />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
---
+++ taskmanagement/src/pages/Routerequests.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Routerequests() {
+  return (
+    <div>Routerequests</div>
+  )
+}
