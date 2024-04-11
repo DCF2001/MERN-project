83bdf843ae0357b3edada500bbc8cb26ff60f91a
Sithmi-Shehara <139143701+Sithmi-Shehara@users.noreply.github.com>
2024-04-11 20:53:13 +0530

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,16 @@ const userSchema = new mongoose.Schema({
         required: true,
         
     },
-}, { timestamps: true});
+    avatar:{
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+
+    },
+
+    
+}, 
+   { timestamps: true}
+);
 
 const User = mongoose.model('User', userSchema);
 
