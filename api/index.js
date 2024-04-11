1a05def1b5fb0a91a64d9213fbde38b6006565c3
DCF2001 <154993535+DCF2001@users.noreply.github.com>
2024-04-11 13:01:52 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to the MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
-const PORT = 5004; // Change the port number to 5000 or any other available port
+const PORT = 5003; // Change the port number to 5000 or any other available port
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} !!!`);
