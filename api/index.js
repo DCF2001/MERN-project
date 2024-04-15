7f4555533a67af470a80c86a900d63ccc61a3cce
Sakunika Ganepola <sakunikaganepola@gmail.com>
2024-04-15 07:27:58 +0530

    Connect to the database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    
+console.log('Connected to mongoDB!');
+}).catch((err) => {
+    console.log(err); 
+});
+
+//express app
 const app = express();
 
+//routes
+app.get('/', (req, res) => {
+    res.json({mssg:'Welcome to the app'})
+
+})
+
+
+//listen for requests
 app.listen(3000,() => {
-    console.log('Server is running on port 3000!');
+    console.log('Server is running on port 3000');
 
 }
 
-);
+)
