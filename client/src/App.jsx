24222c3ebe6373d302db7414b41dd09b37ed8cac
IT22584854-Shehan <it22584854@my.sliit.lk>
2024-04-16 17:22:59 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn"
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import CreateListing from "./pages/CreateListing";
+
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>    
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} /> 
+       <Route path="/profile" element={<Profile />} />
+       <Route path='/create-listing' element ={<CreateListing/>} />        
+    
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+
+
+    
