dbf9d1786365e51653a5669602ca6d9d6219d4c6
DasuWijesu <127611557+DasuWijesu@users.noreply.github.com>
2024-04-18 19:22:08 +0530

    Connect to database

--- Mern_Dasu/api/index.js
+++ Mern_Dasu/api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to the MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-});// we can use any port num like 4000, 5000
+const PORT = process.env.PORT || 5000; // Use the PORT environment variable or default to 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} !!!`);
+});
