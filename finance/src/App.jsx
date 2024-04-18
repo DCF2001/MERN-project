708ddb4ae619b78b384d18a016960b1136196dbc
Devindi Chandrasekara <152923663+RIVIKIRINI@users.noreply.github.com>
2024-04-18 21:28:31 +0530

    create header component

--- finance/src/App.jsx
+++ finance/src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import Reports from "./pages/Reports";
 import Requests from "./pages/Requests";
 import Payments from "./pages/Payments";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/requests" element={<Requests />} />
---
+++ finance/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  return (
+    < header style={{ backgroundColor: '#114232' }} className='shadow-md'>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-8">
+        <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-2xl flex flex-wrap'>
+            <span className='text-slate-500' style={{ color: '#FCDC2A' }}>Green Bin Garbage Management</span>
+        
+        </h1>
+        </Link>
+        {/*<form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+          <input type="text" placeholder="Search..." 
+          className='bg-transparent focus:outline-none w-64 sm:w-64'/>
+          <FaSearch className='text-slate-600'/>
+  </form>*/}
+
+  <ul className='flex gap-8'> 
+  <Link to='/'>
+    <li className='text-sm sm:text-xl hidden sm:inline text-slate-700 hover:underline' style={{ color: '#FFFFFF' }}>Home</li>
+    </Link>
+    <Link to='/payments'>
+    <li className='text-sm sm:text-xl hidden sm:inline text-slate-700 hover:underline' style={{ color: '#FFFFFF' }}>Payments</li>
+    </Link>
+    <Link to='/reports'>
+    <li className='text-sm sm:text-xl hidden sm:inline text-slate-700 hover:underline' style={{ color: '#FFFFFF' }}>Reports</li>
+    </Link>
+    <Link to='/requests'>
+    <li className='text-sm sm:text-xl hidden sm:inline text-slate-700 hover:underline' style={{ color: '#FFFFFF' }}>Requests</li>
+    </Link>
+  </ul>
+        </div>
+    </header>
+  )
+}
