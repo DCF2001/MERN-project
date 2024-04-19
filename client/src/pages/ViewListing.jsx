7c3dbd9392ff356cef3a77224f1b12a6fa43cf83
IT22584854-Shehan <it22584854@my.sliit.lk>
2024-04-19 23:55:22 +0530

    complete delete listing UI and API

--- client/src/pages/ViewListing.jsx
+++ client/src/pages/ViewListing.jsx
@@ -31,6 +31,29 @@ export default function AllListings() {
     fetchListings();
   }, []);
 
+  
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
               {userListings && userListings.length > 0 && (
@@ -60,9 +83,20 @@ export default function AllListings() {
                 to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
-              </Link>
+              </Link>           
 
-            
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+
             </div>
           ))}
         </div>
