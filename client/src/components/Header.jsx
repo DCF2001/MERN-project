d27b4478b2c7abea12ffbffa0945808bb72c305d
Sithmi-Shehara <139143701+Sithmi-Shehara@users.noreply.github.com>
2024-04-20 14:18:35 +0530

    header font color changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,11 +26,11 @@ export default function Header() {
 
         <ul className='flex gap-4'>
         <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-green-800 font-bold hover:underline'>
                 Home</li>
         </Link>
         <Link to='/about'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
+            <li className='hidden sm:inline text-green-800 font-bold hover:underline'>
                 About</li>
         </Link>
         <Link to='/profile'>
@@ -38,7 +38,7 @@ export default function Header() {
             <img className='rounded-full h-7 w-7 object-cover' src= {currentUser.avatar} alt='profile' />
 
 
-        ): (<li className=' text-slate-700 hover:underline'> Sign in</li>
+        ): (<li className=' text-green-800 font-bold hover:underline'> Sign in</li>
         )}
          </Link>
         
