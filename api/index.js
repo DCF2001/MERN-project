05ddd022f2dc95d47e11dd6a4cb4f1720068d5ad
Kavinga17 <kavingaaluvihare2001@gmail.com>
2024-05-05 00:52:43 +0530

    test

--- api/index.js
+++ api/index.js
@@ -2,14 +2,23 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userResearch from './routes/Research.route.js';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 // Increase the size of payload
 app.use(express.urlencoded({ extended: true, parameterLimit: 100000, limit: '1000mb' }));
 app.use(express.json({ limit: '1000mb' }));
+
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,6 +31,10 @@ mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology:
 
 
 app.use('/api/research', userResearch);
+
+app.use('/api/user' , userRouter);
+app.use('/api/auth' ,authRouter);
+
 
 
 const PORT = process.env.PORT || 3000;
